1730ba5d832e12144dea7e538d8eea3b026fe120
chenchaocc <ccerdong@sina.cn>
2015-06-11 23:06:59 +0800

    init
    项目初始化

---
+++ NSLogLocale/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSLogLocale
+//
+//  Created by mac on 15/6/11.
+//  Copyright (c) 2015年 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
